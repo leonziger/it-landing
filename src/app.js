d661cb70ce26af290b5e2f89fa1408ccc6151a63
Uma <buldog52@gmail.com>
2020-07-15 12:33:01 +0300

    Changed right menu

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ window.$ = window.jQuery = require('jquery');
 
 import './components/form-styler/form-styler';
 import './components/menu-toggle/menu-toggle';
-import './components/dropdown/dropdown';
+import './components/the-dropdown/the-dropdown';
 import './components/faq/faq';
 import './components/scroll-to-anchor/scroll-to-anchor';
 import './components/scroll-up/scroll-up';
